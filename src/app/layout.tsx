8d237e80be40f236a6bb4a3e5135acf84f3af60f
vincentCYS <vincent77674@gmail.com>
2025-05-03 21:58:37 +0800

    add display ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,20 @@ export default function RootLayout({
 				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4601799641974929"
 				crossOrigin="anonymous"
 			></script>
+			<script
+				async
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4601799641974929"
+				crossOrigin="anonymous"
+			></script>
+			<ins
+				className="adsbygoogle"
+				style={{ display: "block" }}
+				data-ad-client="ca-pub-4601799641974929"
+				data-ad-slot="4069763015"
+				data-ad-format="auto"
+				data-full-width-responsive="true"
+			></ins>
+			<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				{children}
 			</body>
